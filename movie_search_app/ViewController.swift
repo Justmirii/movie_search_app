e3194d9b4aebb237ee25daa94b683d5f9bc6955e
Justmirii <feyziyevmirmusa@gmail.com>
2023-03-10 14:22:37 +0400

    Initial Commit

---
+++ movie_search_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movie_search_app
+//
+//  Created by Mirmusa Feyziyev on 10.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
